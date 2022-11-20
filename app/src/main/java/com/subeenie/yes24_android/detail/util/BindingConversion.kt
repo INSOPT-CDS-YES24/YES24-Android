896f98642887ea791bb91ff6e7cf28e051f4c1b9
IslandofDream <rhffldkt0517@naver.com>
2022-11-21 01:14:49 +0900

    [feature/#3] add shape / bindingConversion for spannable text

--- app/src/main/java/com/subeenie/yes24_android/detail/util/BindingConversion.kt
+++ app/src/main/java/com/subeenie/yes24_android/detail/util/BindingConversion.kt
@@ -1,14 +1,35 @@
 package com.subeenie.yes24_android.detail.util
 
+import android.graphics.Color
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.style.ForegroundColorSpan
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.databinding.BindingAdapter
+
 
 @BindingAdapter("app:load_price_rank")
 fun ImageView.loadPriceRank(foodName: String) {
     this.setImageResource(PriceRank.valueOf(foodName).imageRes)
 }
 
-@BindingAdapter("app:imgRes")
+@BindingAdapter("app:img_res")
 fun ImageView.loadCastPicture(resid: Int) {
     this.setImageResource(resid)
+}
+
+/**
+ * 사용가능한 할인쿠폰 2개 에서 '2' 부분만 파란색 적용
+ */
+@BindingAdapter("app:spannable_text")
+fun TextView.setSpannable(text: String) {
+    val str = SpannableString(text)
+    str.setSpan(
+        ForegroundColorSpan(Color.parseColor("#508BFF")),
+        text.length - 2,
+        text.length -1,
+        Spanned.SPAN_EXCLUSIVE_INCLUSIVE
+    )
+    this.text = str
 }
