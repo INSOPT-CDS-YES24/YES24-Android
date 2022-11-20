fc129521e54280adb039499baf7957bb6c49fc5e
IslandofDream <rhffldkt0517@naver.com>
2022-11-21 00:37:15 +0900

    [feature/#3] feat adapter / add recyclerview in xml - 어댑터 연결 및 xml 에 컴포넌트 추가

--- app/src/main/java/com/subeenie/yes24_android/detail/DetailActivity.kt
+++ app/src/main/java/com/subeenie/yes24_android/detail/DetailActivity.kt
@@ -4,15 +4,32 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.subeenie.yes24_android.R
 import com.subeenie.yes24_android.databinding.ActivityDetailBinding
+import com.subeenie.yes24_android.detail.adapter.CastAdapter
+import com.subeenie.yes24_android.detail.data.CastData
 
 class DetailActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityDetailBinding
+    private lateinit var adapter: CastAdapter
+    private val castList = listOf<CastData>(
+        CastData("정욱진",R.drawable.img_cast1),
+        CastData("최민우",R.drawable.img_cast2),
+        CastData("렌",R.drawable.img_cast3),
+        CastData("라키",R.drawable.img_cast4),
+        CastData("경윤",R.drawable.img_cast5),
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.castcount = castList.size
+        adapter = CastAdapter(this)
+        binding.rcvCast.adapter = adapter
+        adapter.submitList(castList)
     }
+
+
+
 }
