cb873d965437f032eb1c28e3acbc2a537abe7b2c
IslandofDream <rhffldkt0517@naver.com>
2022-11-25 15:27:15 +0900

    [feature/#15] apply bottom navigation graph

--- app/src/main/java/com/subeenie/yes24_android/MainActivity.kt
+++ app/src/main/java/com/subeenie/yes24_android/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.subeenie.yes24_android
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.subeenie.yes24_android.databinding.ActivityMainBinding
-import com.subeenie.yes24_android.presentation.home.HomeFragment
-import com.subeenie.yes24_android.presentation.mypage.MyticketFragment
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -13,5 +13,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setNavigation()
+    }
+
+    private fun setNavigation() { //
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.home_container) as NavHostFragment
+        val navController = navHostFragment.navController
+        val navGraph = navController.navInflater.inflate(R.navigation.bottom_navigation_graph)
+        navController.graph = navGraph
+        binding.bnv.setupWithNavController(navController)
     }
 }
